6be285678e4f6cf0859498fd88a3182fb946b089
srinitha1478 <srinitha43@gmail.com>
2026-02-01 13:03:39 +0530

    feature-extraction.py

---
+++ feature-extraction.py
@@ -0,0 +1,7 @@
+import librosa
+import numpy as np
+
+def extract_features(file_path):
+    y, sr = librosa.load(file_path, duration=3, offset=0.5)
+    mfcc = librosa.feature.mfcc(y=y, sr=sr, n_mfcc=40)
+    return np.mean(mfcc.T, axis=0)
